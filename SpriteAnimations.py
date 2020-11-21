a8f1b0d701ad975a1918afdf1aeba0f7a7161149
Mdelafuen <mdelafuentemartinez@student.bridgew.edu>
2020-11-21 15:41:53 -0500

    added more things such as random gold and mouse control

--- SpriteAnimations.py
+++ SpriteAnimations.py
@@ -1,8 +1,9 @@
 import arcade
 import Comp151Window
 import types
+import random
 
-def update(window):
+def update(window, delta_time):
     window.player.center_x += window.player.Dx
     window_size = window.get_size()
     if window.player._get_right() > window_size[0] or window.player._get_left() <0:
@@ -10,23 +11,34 @@ def update(window):
 
 
 def draw(window_being_updated):
-    update(window_being_updated)
+    #update(window_being_updated)
     arcade.start_render()
     window_being_updated.player.draw()
+    for gold_pile in window_being_updated.goldList:
+        gold_pile.draw()
 
 def setup_window(graphicsWindow):
+    width, height = graphicsWindow.get_size()
+    gold_list = []
+    for gold_count in range(6):
+        gold_pot = arcade.Sprite("gold-coins-large.png")
+        xPos = random.randint(0, width)
+        yPos = random.randint(0, height)
+        gold_pot.set_position(xPos, yPos)
+        gold_list.append(gold_pot)
     player = arcade.Sprite("galleon.png")
-
     player.set_position(200, 400)
     graphicsWindow.player = player
     arcade.set_background_color(arcade.color.EGYPTIAN_BLUE)
     graphicsWindow.player.Dx = 3
+    graphicsWindow.goldList = gold_list
 
 
 def main():
-    graphicsWindow = Comp151Window.Comp151Window(800, 800, "Ship Demo")
+    graphicsWindow = Comp151Window.Comp151Window(1000, 800, "Ship Demo")
     setup_window(graphicsWindow)
     graphicsWindow.on_draw = types.MethodType(draw, graphicsWindow)
+    graphicsWindow.on_update = types.MethodType(update, graphicsWindow)
     arcade.run()
 
 
